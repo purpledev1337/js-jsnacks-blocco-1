1c2cabdfe3092824240bc8f884816cb622b82388
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-10-28 15:04:21 +0200

    js1- done with console log

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,2 +1,16 @@
 // 1 - L’utente inserisce due numeri in successione, con due prompt.
+const firstNum = parseInt(prompt("Inserisci il primo numero"));
+const secondNum = parseInt(prompt("Inserisci il secondo numero"));
+
+console.log("Il primo numero scelto è: ", firstNum);
+console.log("Il secondo numero scelto è: ", secondNum);
+
 // Il software stampa il maggiore.
+let maxNum;
+if (firstNum > secondNum) {
+    maxNum = firstNum;
+} else {
+    maxNum = secondNum;   
+}
+
+console.log("Il numero maggiore è: ", maxNum);
